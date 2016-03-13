e6bc3c138fc5dffd196faf94db808c30303c11ff
Albert T <electron826@gmail.com>
2016-03-13 18:36:27 +0000

    Now uses cookies/sessions, and modifies the client view accordingly.

--- queue/views.py
+++ queue/views.py
@@ -3,6 +3,7 @@ from django.template import loader
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import permission_required
+from django.contrib.sessions.models import Session
 from .models import Queue
 
 # Create your views here.
@@ -11,10 +12,14 @@ def client_view(request):
     record = Queue.objects.all()[0]
     
     template = loader.get_template('queue/client.html')
+
+    queue_position = request.session['queue_position'] if ( 'queue_position' in request.session ) else None
     
     context = {
         'queue_head' : record.head,
-        'queue_tail' : record.tail
+        'queue_tail' : record.tail,
+
+        'queue_position' : queue_position  # Actually a cookie
         }
     
     return HttpResponse(template.render(context, request))
@@ -47,8 +52,9 @@ def increment_tail(request):
     record = Queue.objects.all()[0]   # This is the value from the database
 
     record.tail += 1     # We add one to it,
-    
     record.save()   # And then we save it.
+
+    request.session['queue_position'] = record.tail
     
     #
     return HttpResponseRedirect( reverse('queue:client_view') )
@@ -60,6 +66,8 @@ def reset(request):
     record.tail = 0
     
     record.save()   # And then we save it.
+
+    Session.objects.all().delete() # Delete all the sessions (like cookies)
     
     #
     return HttpResponseRedirect( reverse('queue:staff_view') )
